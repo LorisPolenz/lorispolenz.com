d19d0962841e7524219685d4b3da4f9e20874112
Loris Polenz <contact@lorispolenz.com>
2022-06-28 18:55:57 +0200

    add fist content to page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,109 +1,27 @@
 import * as React from "react"
+import * as indexStyles from '../styles/pages/Index.module.css'
+
+
+
+//data
 
 // styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  }
-]
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
 
 // markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
+    <main>
       <title>Loris Polenz</title>
-      <h1 style={headingStyles}>
-        Welcome to my page! Be aware that this page is under construction! If you want to reach out to me   <a href="mailto:loris@lorispolenz.com">loris@lorispolenz.com</a>
-        <br />
-        <br />
-        <br />
-        <span style={headingAccentStyles}>— Thanks for visiting this page! </span>
-        🎉🎉🎉
-      </h1>
+      <h1>Hi!</h1>
+      <h3>I'm Loris, feel free to reach out to me! (<a href="mailto:loris@lorispolenz.com">loris@lorispolenz.com</a>)</h3>
 
+      <div className={indexStyles.links}>
+        <h2>Links</h2>
+        <a href="https://github.com/lorispolenz">GitHub</a>
+        <a href="https://www.linkedin.com/in/lorispolenz/">LinkedIn</a>
+        <a href="./projects">Projects</a>
+        <a href=".">this</a>
+      </div>
     </main>
   )
 }
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -4,30 +4,42 @@ import ProjectTemplate from "../templates/ProjectTemplate"
 interface Project {
     title: String,
     description: String,
-    image: URL,
+    image?: URL,
+    link?: URL,
     technologiesUsed: Array<String>
 }
 
 //data
 let projects: Project[] = [
-    // {
-    //     title: "",
-    //     description: "",
-    //     image: new URL("https://images.pexels.com/photos/12405196/pexels-photo-12405196.jpeg"),
-    //     technologiesUsed: ["TS", "React", "Docker"]
-    // },
-    // {
-    //     title: "ProjectTitle",
-    //     description: "ProjectDescription",
-    //     image: new URL("https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg"),
-    //     technologiesUsed: ["TS", "React", "Docker"]
-    // }
+    {
+        title: "Pwn-pass",
+        description: "Pwm-pass is a joke project leaning to the fact that some people use very insecure passwords. To help these people find their perfect insecure password I created pwn-pass based on a leaked password list.",
+        link: new URL("https://github.com/lorispolenz/pwn-pass"),
+        technologiesUsed: ["React", "Go", "MongoDB"]
+    },
+    {
+        title: "Gym Tracker",
+        description: "My gym has a public API with the people currently in the gym. This project harvests this data every 5 minutes and saves it to a CSV file. This data then can be used to determine the best time of the day for a good gym session.",
+        link: new URL('https://github.com/lorispolenz/gym-tracker'),
+        technologiesUsed: ["Python", "Docker"]
+    },
+    {
+        title: "Traily",
+        description: "At Traily my main job was to manage the team and support then during the whole Scrum process. ",
+        link: new URL('https://traily.ch'),
+        technologiesUsed: ['Scrum']
+
+    }
 ]
+
+
+
 
 // markup
 const ProjectPage = () => {
     return (
         <main>
+            <title>Projects</title>
             <h1>These are the sick page projects</h1>
             {projects.map((project, index) =>
                 <ProjectTemplate props={project} key={index}></ProjectTemplate>
